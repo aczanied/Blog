5e0b243917f19782816540670e41fab9e3bec5c3
Eduardo Acosta <aczanied@gmail.com>
2021-08-14 20:14:29 -0500

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminListComponent } from './pages/admin/admin-list/admin-list.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  // Routes of Template
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './pages/main/main.component';
+import { ListComponent } from './pages/admin/list/list.component';
+import { AdminListComponent } from './pages/admin/admin-list/admin-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ListComponent,
+    AdminListComponent
   ],
   imports: [
     BrowserModule,
